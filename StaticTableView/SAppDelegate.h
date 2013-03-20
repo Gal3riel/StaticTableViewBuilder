886f7e4882ad99b551869e081845b38daf7649ba
Gabriel <Gal3rielol@Gmail.com>
2013-03-20 19:48:31 +0800

    Initial Commit

---
+++ StaticTableView/SAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SAppDelegate.h
+//  StaticTableView
+//
+//  Created by Suen Gabriel on 3/20/13.
+//  Copyright (c) 2013 Gabriel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ StaticTableView/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  StaticTableView
+//
+//  Created by Suen Gabriel on 3/20/13.
+//  Copyright (c) 2013 Gabriel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SAppDelegate class]));
+    }
+}
